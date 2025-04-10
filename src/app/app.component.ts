21fc6311c64f33cf5a7333adcc57973ff6760992
Subhro-ai <subhrajyoti.singha30@gmail.com>
2025-04-10 13:50:49 +0530

    Feat: Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, ButtonModule, MenubarModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+        {
+            label: 'Home',
+            icon: 'pi pi-home',
+        },
+        {
+          label : 'About',
+          icon : 'pi pi-user'
+        },
+        {
+          label: 'Skills',
+          icon : 'pi pi-lightbulb'
+        },
+        {
+            label: 'Projects',
+            icon: 'pi pi-search',
+        },
+        {
+          label : 'Contact',
+          icon : 'pi pi-inbox'
+        }
+    ];
 }
+
+  toggleDarkMode() {
+    const element : any = document.querySelector('html');
+    element.classList.toggle('my-app-dark');
+}
+
+}
+
+
